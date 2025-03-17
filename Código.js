d0ed13bbe30ac576e7aed26f05e98f26467f912b
Matheus Felipe Alves Santos <santos0508matheus@outlook.com>
2025-03-17 17:29:48 -0300

    Adicionado contador de execuções

--- Código.js
+++ Código.js
@@ -4,4 +4,16 @@ function escreverMensagem() {
   
   aba.getRange("A1").setValue("Olá, GitHub!");
   aba.getRange("B1").setValue("Última execução: " + new Date());
+  
+  // Contador de execuções na célula C1
+  var contadorRange = aba.getRange("C1");
+  var contadorAtual = contadorRange.getValue();
+  
+  if (!contadorAtual) {
+    contadorAtual = 1;
+  } else {
+    contadorAtual++;
+  }
+
+  contadorRange.setValue(contadorAtual);
 }
