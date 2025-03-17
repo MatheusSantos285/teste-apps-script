5a164e76abee919fe8ef6356224ddb8b144a9ca1
Matheus Felipe Alves Santos <santos0508matheus@outlook.com>
2025-03-17 17:02:16 -0300

    Adicionada data/hora na célula B1 ao executar a função

--- Código.js
+++ Código.js
@@ -1,5 +1,7 @@
 function escreverMensagem() {
   var planilha = SpreadsheetApp.getActiveSpreadsheet();
   var aba = planilha.getActiveSheet();
+  
   aba.getRange("A1").setValue("Olá, GitHub!");
+  aba.getRange("B1").setValue("Última execução: " + new Date());
 }
